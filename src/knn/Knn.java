6034fedac1116dac95122281282d2ce5286d8b83
João Victor <jvictor2012@outlook.com>
2019-10-23 12:55:42 -0300

    Commit KNN(incompleto)

--- src/knn/Knn.java
+++ src/knn/Knn.java
@@ -6,112 +6,25 @@
 package knn;
 
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 /**
  *
- * @author Jhonatan
+ * @author Jhonatan, João Victor
  */
 public class Knn {
-
-    /**
-     * @param args the command line arguments
-     */
-    //Declaração dos 3
-    /*private Float distanciaDoisPontos(float[] Elementos1, float[] Elementos2) {
-    int x1 =  Math.round(Elementos1[XCoordenada]);
-    int y1 =  Math.round(Elementos1[YCoordenada]);
-    int z1 =  Math.round(Elementos1[ZCoordenada]);
-    int x2 =  Math.round(Elementos2[XCoordenada]);
-    int y2 =  Math.round(Elementos2[YCoordenada]);
-    int z2 =  Math.round(Elementos2[ZCoordenada]);
-    int t1 = (x2 - x1) * (x2 - x1);
-    int t2 = (y2 - y1) * (y2 - y1);
-    int t3 = (z2 - z1) * (z2 - z1);
-    int soma = t1 + t2 + t3;
-    String somaConv = Integer.toString(soma);
-    double somaConvD = Double.parseDouble(somaConv);
-    double distancia = Math.abs (Math.sqrt (somaConvD) );
-    String distanciaConv = Double.toString(distancia);
-    return Float.parseFloat(distanciaConv);
-    }
     
-    private int achaK () {
-    tam = data.tam();
-    String tamString = Integer.toString( tam ) ;
-    double tamDouble = Double.parseDouble( tamString );
-    double distDouble = Math.sqrt( tamDouble );
-    double rawK = distDouble / 2 ;
-    int num = Math.round( ( float )rawK ) ;
-    if ( num%2 != 0 ) {
-        return num ;
-    }
-    else {
-        return num - 1 ;
-    }
-    }
-    */
+    public static int k = 3;
     
-    
-    
-    public static int k = 5;
-    
+  
+    //calculo distância euclidiana
     public static double distance(PontoKnn o, PontoKnn ref){
-        return (((o.x - ref.x) * (o.x - ref.x)) + ((o.y - ref.y) * (o.y - ref.y)) + ((o.z - ref.z)*(o.z - ref.z)));
+        return (((o.getX() - ref.getX()) * (o.getX() - ref.getX())) + ((o.getY() - ref.getY()) * (o.getY() - ref.getY())) + ((o.getZ() - ref.getZ())*(o.getZ() - ref.getZ())));
     }
     
     
     public static int kNN(PontoKnn o, ArrayList<PontoKnn> p){
-        int[] indices = new int[k];
-        double[] mins = new double[k];
-        for (int i=0; i<k; i++){
-            mins[i] = Double.MAX_VALUE;
-        }
         
-        for(int i=0; i<p.size(); i++){
-            double dist = distance(o, p.get(i));
-            double max = Double.MIN_VALUE;
-            int maxID = 0;
-            for(int j=0; j<k; j++){
-                if(max<mins[j]){
-                    max=mins[j];
-                    maxID = j;
-                }
-                if(mins[maxID] > dist) {
-                    mins[maxID] = dist;
-                    indices[maxID] = i;
-                }
-            }
-        }
-            
-            int a=0;
-            int b=0;
-            
-            for(int i=0; i<k; i++){
-                if(p.get(indices[i]).classe == 0){
-                    a++;
-                }else{
-                    b++;
-                }
-            }
-            
-                    
-            if(a>b){
-                
-                return 1;
-                o.classe = (1)
-                /*Amostra nova:
-                10,20,30
-                Classe: 1 (100%)
-                k (3) mais similares:
-                10,20,30,1 (100 %)
-                10,20,25,1 (90 %)
-                5,10,25,1 (50 %)*/
-            }else{
-                System.out.println("Amostra Nova : "+o.x+","+o.y+","+o.z);
-                System.out.println("Classe : "+o.classe);
-                
-                return 2;
-            }
     }
     /*
     1,10,20,30,1
@@ -131,24 +44,8 @@ public class Knn {
 	p.add(new PontoKnn(5,10,25,1));
         
         PontoKnn o = new PontoKnn(10,20,30);
-        
-        System.out.println(kNN(o, p));
+        System.out.println(kNN(o,p));
     }
-        /*double x1,y1,x2,y2;
-        double p1,p2, distancia;
-        Scanner leitor = new Scanner(System.in);
-        System.out.println("Digite o x1: ");
-        x1 = leitor.nextDouble();
-        System.out.println("Digite o y2: ");
-        y1 = leitor.nextDouble();
-        System.out.println("Digite o x2: ");
-        x2 = leitor.nextDouble();
-        System.out.println("Digite o y2: ");
-        y2 = leitor.nextDouble();
-        
-        p1 = Math.pow(x2 - x1, 2);
-        p2 = Math.pow(y2 - y1, 2);
-        
-        distancia = Math.sqrt(p1+p2);*/
+    
     
 }
--- src/knn/PontoKnn.java
+++ src/knn/PontoKnn.java
@@ -10,7 +10,10 @@ package knn;
  * @author JVict
  */
 public class PontoKnn {
-    int x, y, z, classe;
+    private int x;
+    private int y;
+    private int z;
+    private int classe;
     
     public PontoKnn(int x,int y, int z){
         this.x = x;
@@ -22,6 +25,62 @@ public class PontoKnn {
         this.x = x;
         this.y = y;
         this.z = z;
+        this.classe = classe;
+    }
+
+    /**
+     * @return the x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * @param x the x to set
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * @return the y
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * @param y the y to set
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    /**
+     * @return the z
+     */
+    public int getZ() {
+        return z;
+    }
+
+    /**
+     * @param z the z to set
+     */
+    public void setZ(int z) {
+        this.z = z;
+    }
+
+    /**
+     * @return the classe
+     */
+    public int getClasse() {
+        return classe;
+    }
+
+    /**
+     * @param classe the classe to set
+     */
+    public void setClasse(int classe) {
         this.classe = classe;
     }
 }
